8e983bd91d43c380510f0ffd8e94fb6c55f4dee2
TimothyAK <mothyabraham973@gmail.com>
2025-05-19 22:11:35 +0700

    Applied loading animation in ForgotPass Form

--- front-end/src/components/organism/ForgotPassForm/ForgotPassForm.jsx
+++ front-end/src/components/organism/ForgotPassForm/ForgotPassForm.jsx
@@ -7,17 +7,21 @@ import './ForgotPassForm.css';
 
 const ForgotPassForm = () => {
   const [userEmail, setUserEmail] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
   const navigate = useNavigate();
 
   const handleReset = async (e) => {
     e.preventDefault();
     try {
         console.log("Sending reset link...");
+        setIsLoading(true)
         const requestResetResponse = await requestResetAPI(userEmail)
         localStorage.setItem("requestId", userEmail)
+        setIsLoading(false)
         navigate('/login');
     } catch (err) {
         // Display error message. Bisa dipake buat show error di form.
+        setIsLoading(false)
         console.log(err.response.data.message)
     }
   };
@@ -35,7 +39,7 @@ const ForgotPassForm = () => {
         required
       />
 
-      <NextButton type="submit">Send Reset Link</NextButton>
+      <NextButton type="submit" isLoading={isLoading}>Send Reset Link</NextButton>
     </form>
   );
 };
