0197469f504fe004b2c023574b7e2926169ac948
TimothyAK <mothyabraham973@gmail.com>
2025-05-19 22:09:56 +0700

    Removed UserContext Reference, Deleted UserContext

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -2,15 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; 
 import './index.css';
-import UserProvider from './context/UserContext.jsx';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <UserProvider>
-        <BrowserRouter> 
-            <App />
-        </BrowserRouter>
-    </UserProvider>
+    <BrowserRouter> 
+        <App />
+    </BrowserRouter>
   </StrictMode>
 );
