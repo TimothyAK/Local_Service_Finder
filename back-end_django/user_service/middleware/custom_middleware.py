35ad61419a90418fcf144977712e9f88a6a7c3f6
TimothyAK <mothyabraham973@gmail.com>
2025-05-19 13:41:55 +0700

    Added custom middleware, Modified login endpoint to return the data in JWT

---
+++ back-end_django/user_service/middleware/custom_middleware.py
@@ -0,0 +1,43 @@
+import os
+import jwt
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
+
+class JWTMiddleware():
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.JWT_SECRET = os.getenv("JWT_SECRET") 
+
+    def __call__(self, request):
+        try:
+            if(request.isProtected and request.headers['Jwt']):
+                print("Verifying JWT")
+                jwtPayload = jwt.decode(request.headers['Jwt'], self.JWT_SECRET, 'HS256')
+                request.data = jwtPayload
+
+            response = self.get_response(request)
+            return response
+        except Exception as e:
+            response = Response({
+                "message": "Invalid JWT"
+            }, status=401)
+            response.accepted_renderer = JSONRenderer()
+            response.accepted_media_type = "application/json"
+            response.renderer_context = {}
+            response.render()
+            return response
+
+class RouteProtectionMiddleware():
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.protected_routes = ['delete_account'] 
+
+    def __call__(self, request):
+        print("Setting protected route validation variable")
+        request.isProtected = False
+        for route in self.protected_routes:
+            if route in request.path:
+                request.isProtected = True
+
+        response = self.get_response(request)
+        return response
