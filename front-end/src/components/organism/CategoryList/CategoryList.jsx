5e0ee9919d73395919f71425dce4e732a2330fa5
TimothyAK <mothyabraham973@gmail.com>
2025-05-29 16:13:27 +0700

    Added custom onclick function to each category list buttons

--- front-end/src/components/organism/CategoryList/CategoryList.jsx
+++ front-end/src/components/organism/CategoryList/CategoryList.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import CategoryItem from "../../molecules/CategoryItem/CategoryItem";
+import { nearbySearchAPI } from "../../../api/amenityAPI";
 import "./CategoryList.css";
+import { useNavigate } from "react-router-dom";
 
 const categories = [
-  { icon: "restaurant", label: "F&B", to: "/service", serviceType: "F&B" },
-  { icon: "account_balance", label: "Finance", to: "/service", serviceType: "Finance" },
-  { icon: "shopping_cart", label: "Shopping", to: "/service", serviceType: "Shopping" },
-  { icon: "local_hospital", label: "Healthcare", to: "/service", serviceType: "Healthcare" },
-  { icon: "movie", label: "Entertainment", to: "/service", serviceType: "Entertainment" }
+  { icon: "restaurant", label: "F&B", to: "/service", serviceType: "FNB" },
+  { icon: "account_balance", label: "Finance", to: "/service", serviceType: "FINANCE" },
+  { icon: "shopping_cart", label: "Shopping", to: "/service", serviceType: "SHOPPING" },
+  { icon: "local_hospital", label: "Healthcare", to: "/service", serviceType: "HEALTHCARE" },
+  { icon: "movie", label: "Entertainment", to: "/service", serviceType: "ENTERTAINMENT" }
 ];
 
+// () => 
+
 function CategoryList() {
+    const navigate = useNavigate()
   return (
     <div className="CategoryList">
       {categories.map((category, index) => (
@@ -18,8 +23,15 @@ function CategoryList() {
           key={index} 
           icon={category.icon} 
           label={category.label} 
-          to={category.to}
-          state={{ serviceType: category.serviceType }}
+          onClick={async () => {
+            const userLoc = JSON.parse(localStorage.getItem("userLoc"))
+            const searchResult = await nearbySearchAPI(category.serviceType, userLoc["latitude"], userLoc["longitude"], localStorage.getItem("userJWT"))
+            navigate(category.to, { 
+                state: {
+                    searchResult: searchResult["data"]
+                }
+            })
+          }}
         />
       ))}
     </div>
