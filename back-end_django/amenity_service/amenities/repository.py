4078668b3bfdfb99187f0d6dd6ccbab265a31dc5
TimothyAK <mothyabraham973@gmail.com>
2025-05-29 14:10:12 +0700

    Fixed getQueryString function, Normalize variable names, Removed unused imported package

--- back-end_django/amenity_service/amenities/repository.py
+++ back-end_django/amenity_service/amenities/repository.py
@@ -1,84 +1,83 @@
 import requests
-import json
 
 class AmenityRepository:
     def __init__(self):
         self.__base_url = "https://overpass-api.de/api/interpreter"
 
-    def get_amenity_query_string(amenity_category: str, user_range: str):
+    def getQueryString(self, amenity_category, userRange):
         amenityQueryFnB = f"""
         [out:json];
         (
-            node["amenity"~"restaurant|fast_food|cafe|pub|bar|food_court|biergarten|ice_cream"]["name"]{user_range};
-            node["shop"~"bakery|confectionery|butcher|cheese|greengrocer|alcohol|beverages"]["name"]{user_range};
-            way["amenity"~"restaurant|fast_food|cafe|pub|bar|food_court|biergarten|ice_cream"]["name"]{user_range};
-            way["shop"~"bakery|confectionery|butcher|cheese|greengrocer|alcohol|beverages"]["name"]{user_range};
-            relation["amenity"~"restaurant|fast_food|cafe|pub|bar|food_court|biergarten|ice_cream"]["name"]{user_range};
-            relation["shop"~"bakery|confectionery|butcher|cheese|greengrocer|alcohol|beverages"]["name"]{user_range};
+            node["amenity"~"restaurant|fast_food|cafe|pub|bar|food_court|biergarten|ice_cream"]["name"]{userRange};
+            node["shop"~"bakery|confectionery|butcher|cheese|greengrocer|alcohol|beverages"]["name"]{userRange};
+            way["amenity"~"restaurant|fast_food|cafe|pub|bar|food_court|biergarten|ice_cream"]["name"]{userRange};
+            way["shop"~"bakery|confectionery|butcher|cheese|greengrocer|alcohol|beverages"]["name"]{userRange};
+            relation["amenity"~"restaurant|fast_food|cafe|pub|bar|food_court|biergarten|ice_cream"]["name"]{userRange};
+            relation["shop"~"bakery|confectionery|butcher|cheese|greengrocer|alcohol|beverages"]["name"]{userRange};
         );
-        out center tags 10;
+        out center tags 20;
         """
         amenityQueryShopping = f"""
         [out:json];
         (
-            node["shop"~"supermarket|mall|department_store|clothes|shoes|jewelry|optician|beauty|hairdresser|cosmetics|perfumery|books|stationery|electronics|mobile_phone|computer|hardware|furniture|doityourself|florist|gift|toys|sports|outdoor|second_hand|charity|travel_agency|laundry|dry_cleaning"]["name"]{user_range};
-            way["shop"~"supermarket|mall|department_store|clothes|shoes|jewelry|optician|beauty|hairdresser|cosmetics|perfumery|books|stationery|electronics|mobile_phone|computer|hardware|furniture|doityourself|florist|gift|toys|sports|outdoor|second_hand|charity|travel_agency|laundry|dry_cleaning"]["name"]{user_range};
-            relation["shop"~"supermarket|mall|department_store|clothes|shoes|jewelry|optician|beauty|hairdresser|cosmetics|perfumery|books|stationery|electronics|mobile_phone|computer|hardware|furniture|doityourself|florist|gift|toys|sports|outdoor|second_hand|charity|travel_agency|laundry|dry_cleaning"]["name"]{user_range};
+            node["shop"~"supermarket|mall|department_store|clothes|shoes|jewelry|optician|beauty|hairdresser|cosmetics|perfumery|books|stationery|electronics|mobile_phone|computer|hardware|furniture|doityourself|florist|gift|toys|sports|outdoor|second_hand|charity|travel_agency|laundry|dry_cleaning"]["name"]{userRange};
+            way["shop"~"supermarket|mall|department_store|clothes|shoes|jewelry|optician|beauty|hairdresser|cosmetics|perfumery|books|stationery|electronics|mobile_phone|computer|hardware|furniture|doityourself|florist|gift|toys|sports|outdoor|second_hand|charity|travel_agency|laundry|dry_cleaning"]["name"]{userRange};
+            relation["shop"~"supermarket|mall|department_store|clothes|shoes|jewelry|optician|beauty|hairdresser|cosmetics|perfumery|books|stationery|electronics|mobile_phone|computer|hardware|furniture|doityourself|florist|gift|toys|sports|outdoor|second_hand|charity|travel_agency|laundry|dry_cleaning"]["name"]{userRange};
         );
-        out center tags 10;
+        out center tags;
         """
         amenityQueryFinance = f"""
         [out:json];
         (
-            node["amenity"~"bank|atm|bureau_de_change"]{user_range};
-            way["amenity"~"bank|atm|bureau_de_change"]{user_range};
-            relation["amenity"~"bank|atm|bureau_de_change"]{user_range};
+            node["amenity"~"bank|atm|bureau_de_change"]{userRange};
+            way["amenity"~"bank|atm|bureau_de_change"]{userRange};
+            relation["amenity"~"bank|atm|bureau_de_change"]{userRange};
         );
-        out center tags 10;
+        out center tags;
         """
         amenityQueryHealthcare = f"""
         [out:json];
         (
-            node["amenity"~"hospital|clinic|doctors|dentist|pharmacy|health_post|nursing_home|veterinary"]["name"]{user_range};
-            way["amenity"~"hospital|clinic|doctors|dentist|pharmacy|health_post|nursing_home|veterinary"]["name"]{user_range};
-            relation["amenity"~"hospital|clinic|doctors|dentist|pharmacy|health_post|nursing_home|veterinary"]["name"]{user_range};
+            node["amenity"~"hospital|clinic|doctors|dentist|pharmacy|health_post|nursing_home|veterinary"]["name"]{userRange};
+            way["amenity"~"hospital|clinic|doctors|dentist|pharmacy|health_post|nursing_home|veterinary"]["name"]{userRange};
+            relation["amenity"~"hospital|clinic|doctors|dentist|pharmacy|health_post|nursing_home|veterinary"]["name"]{userRange};
         );
-        out center tags 10;
+        out center tags;
         """
         amenityQueryEntertainment = f"""
         [out:json];
         (
-            node["amenity"~"cinema|theatre|nightclub|arts_centre"]["name"]{user_range};
-            node["leisure"~"park|playground|water_park|stadium|sports_centre|ice_rink|golf_course|swimming_pool"]["name"]{user_range};
-            node["tourism"~"theme_park|zoo|attraction|picnic_site|viewpoint"]["name"]{user_range};
-            node["historic"~"castle|monument|memorial|ruins|archaeological_site"]["name"]{user_range};
-            way["amenity"~"cinema|theatre|nightclub|arts_centre"]["name"]{user_range};
-            way["leisure"~"park|playground|water_park|stadium|sports_centre|ice_rink|golf_course|swimming_pool"]["name"]{user_range};
-            way["tourism"~"theme_park|zoo|attraction|picnic_site|viewpoint"]["name"]{user_range};
-            way["historic"~"castle|monument|memorial|ruins|archaeological_site"]["name"]{user_range};
-            relation["amenity"~"cinema|theatre|nightclub|arts_centre"]["name"]{user_range};
-            relation["leisure"~"park|playground|water_park|stadium|sports_centre|ice_rink|golf_course|swimming_pool"]["name"]{user_range};
-            relation["tourism"~"theme_park|zoo|attraction|picnic_site|viewpoint"]["name"]{user_range};
-            relation["historic"~"castle|monument|memorial|ruins|archaeological_site"]["name"]{user_range};
+            node["amenity"~"cinema|theatre|nightclub|arts_centre"]["name"]{userRange};
+            node["leisure"~"park|playground|water_park|stadium|sports_centre|ice_rink|golf_course|swimming_pool"]["name"]{userRange};
+            node["tourism"~"theme_park|zoo|attraction|picnic_site|viewpoint"]["name"]{userRange};
+            node["historic"~"castle|monument|memorial|ruins|archaeological_site"]["name"]{userRange};
+            way["amenity"~"cinema|theatre|nightclub|arts_centre"]["name"]{userRange};
+            way["leisure"~"park|playground|water_park|stadium|sports_centre|ice_rink|golf_course|swimming_pool"]["name"]{userRange};
+            way["tourism"~"theme_park|zoo|attraction|picnic_site|viewpoint"]["name"]{userRange};
+            way["historic"~"castle|monument|memorial|ruins|archaeological_site"]["name"]{userRange};
+            relation["amenity"~"cinema|theatre|nightclub|arts_centre"]["name"]{userRange};
+            relation["leisure"~"park|playground|water_park|stadium|sports_centre|ice_rink|golf_course|swimming_pool"]["name"]{userRange};
+            relation["tourism"~"theme_park|zoo|attraction|picnic_site|viewpoint"]["name"]{userRange};
+            relation["historic"~"castle|monument|memorial|ruins|archaeological_site"]["name"]{userRange};
         );
-        out center tags 10;
+        out center tags;
         """
-        if amenity_category.find("FNB") != -1:
+        if amenity_category == "FNB":
             return amenityQueryFnB
-        elif amenity_category.find("SHOPPING") != -1:
+        elif amenity_category == "SHOPPING":
             return amenityQueryShopping
-        elif amenity_category.find("FINANCE") != -1:
+        elif amenity_category == "FINANCE":
             return amenityQueryFinance
-        elif amenity_category.find("HEALTHCARE") != -1:
+        elif amenity_category == "HEALTHCARE":
             return amenityQueryHealthcare
-        elif amenity_category.find("ENTERTAINMENT") != -1:
+        elif amenity_category == "ENTERTAINMENT":
             return amenityQueryEntertainment
 
 
-    async def getNearbyAmenities(self, userCoordinates, category):
+    def getNearbyAmenities(self, userCoordinates, category):
         userRange = f"(around: 1000, {userCoordinates["lat"]}, {userCoordinates["lon"]})"
-        amenityQuery = self.get_amenity_query_string(category, userRange)
-        searchResponse = await requests.post(self.__base_url, data=amenityQuery)
+        amenityQuery = self.getQueryString(category, userRange)
+        searchResponse = requests.post(self.__base_url, data=amenityQuery)
         return searchResponse.json()["elements"]
 
 
