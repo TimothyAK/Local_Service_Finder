44ac14fe1b36e3da16a10d7347a1bcefea1758a8
TimothyAK <mothyabraham973@gmail.com>
2025-05-29 14:08:56 +0700

    Modified APIView to use sync APIView (rest_framework.views.APIView), Changed exception instance to throw custom error

--- back-end_django/amenity_service/amenities/views.py
+++ back-end_django/amenity_service/amenities/views.py
@@ -1,4 +1,4 @@
-from adrf.views import APIView
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from .service import AmenityService
 
@@ -8,5 +8,30 @@ class NearbySearchController(APIView):
         self.__amenity_service = AmenityService()
 
     def get(self, request):
-        print(request.GET)
-        return Response("OKE", status=200)
+        queryParams = request.GET
+        try:
+            if (not queryParams.get("lat") or not float(queryParams.get("lat")) or float(queryParams.get("lat")) == 0 or
+                not queryParams.get("lon") or not float(queryParams.get("lon")) or float(queryParams.get("lon")) == 0 or
+                not queryParams.get("category") or queryParams.get("category") == ""):
+                raise Exception("Invalid request parameters", 400)
+            
+            searchResult = self.__amenity_service.searchNearbyAmenities(float(queryParams.get("lat")), float(queryParams.get("lon")), queryParams.get("category"))
+
+            return Response({
+                "data": searchResult
+            }, 200)
+        except ValueError as e:
+            return Response({
+                "message": "Invalid requets parameters"
+            }, status=400)
+        except Exception as e:
+            msg = e.args[0]
+
+            try:
+                return Response({
+                    "message": msg
+                }, status=e.args[1])
+            except:
+                return Response({
+                    "message": "Internal server error"
+                }, 500)
