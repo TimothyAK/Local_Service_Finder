ed649da39c88376aa9cab2e19afcf22a192839fb
TimothyAK <mothyabraham973@gmail.com>
2025-05-29 22:58:44 +0700

    Modified service layer

--- back-end_django/user_amenity_service/user_amenity/service.py
+++ back-end_django/user_amenity_service/user_amenity/service.py
@@ -4,10 +4,10 @@ class UserAmenityService:
     def __init__(self):
         self.__userAmenityRepository = UserAmenityRepository()
 
-    def getUserAmenitiesByUserID(self, userID):
-        userAmenities = self.__userAmenityRepository.getUserAmenitiesByUserID(userID)
+    async def getUserAmenitiesByUserID(self, userID):
+        userAmenities = await self.__userAmenityRepository.getUserAmenitiesByUserID(userID)
         return userAmenities
     
-    def createUserAmenity(self, newUserAmenityDoc):
-        result = self.__userAmenityRepository.createUserAmenity(newUserAmenityDoc)
+    async def createUserAmenity(self, newUserAmenityDoc):
+        result = await self.__userAmenityRepository.createUserAmenity(newUserAmenityDoc)
         return result
