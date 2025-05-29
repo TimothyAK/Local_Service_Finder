9edd90099b5ecbd3d371899c11ba5c5a36ff89f7
TimothyAK <mothyabraham973@gmail.com>
2025-05-29 22:28:04 +0700

    Created user amenities model layer

--- back-end_django/user_amenity_service/user_amenity/models.py
+++ back-end_django/user_amenity_service/user_amenity/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from pymongo import AsyncMongoClient
+import os
 
-# Create your models here.
+class UserAmenities:
+    def __init__(self):
+        client = AsyncMongoClient(os.getenv("MONGO_URI"))
+        db = client[os.getenv("MONGO_DB_NAME")]
+        self.__collection = db["UserAmenities"]
+    
+    def getUserAmenitiesCollection(self):
+        return self.__collection
+        
