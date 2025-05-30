cce2214ef0ae0f37b67dc9c5f7e2ad86b55cee41
TimothyAK <mothyabraham973@gmail.com>
2025-05-30 21:58:16 +0700

    Created new component for history page map

---
+++ front-end/src/components/organism/HistoryMap/HistoryMap.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { Map, Marker } from '@vis.gl/react-maplibre';
+import { useLocation } from 'react-router-dom';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import './HistoryMap.css';
+
+// Udah gk dipake
+// const markerData = {
+//   'F&B': [{ lat: 37.7749, lng: -122.4194, title: 'San Francisco - F&B Spot' }],
+//   'Finance': [{ lat: 40.7128, lng: -74.0060, title: 'Wall Street - Finance Center' }],
+//   'Shopping': [{ lat: 37.7842, lng: -122.4074, title: 'Union Square - Shopping' }],
+//   'Healthcare': [{ lat: 37.7694, lng: -122.4064, title: 'UCSF Medical Center' }],
+//   'Entertainment': [{ lat: 37.7694, lng: -122.4862, title: 'Golden Gate Park' }],
+// };
+
+const HistoryMap = () => {
+  const mapRef = useRef(null); 
+  const location = useLocation();
+  const stateSearchResult = location.state?.searchResult || [];       
+  const mapInstanceRef = useRef(null); 
+  const [loaded, setLoaded] = useState(false);
+  const [center, setCenter] = useState({ lat: -6.17545, lng: -106.82702 })
+
+  useEffect(() => {
+    let userLoc = null;
+    try {
+        const userLocJSON = localStorage.getItem('userLoc');
+        userLoc = userLocJSON ? JSON.parse(userLocJSON) : null;
+        setCenter({ lat: userLoc.latitude, lng: userLoc.longitude })
+    } catch {}
+  }, []);
+
+  useEffect(() => {
+    if (loaded && mapInstanceRef.current) {
+      mapInstanceRef.current.flyTo({
+        center: [center.lng, center.lat],
+        zoom: 16,
+        speed: 1.2,
+        curve: 1.5,
+      });
+    }
+  }, [loaded])
+
+  return (
+    <div className="map-container">
+      <Map
+        ref={mapRef}
+        initialViewState={{
+          longitude: center.lng,
+          latitude: center.lat,
+            zoom: 0,
+            speed: 1.2,
+            curve: 1.5,
+        }}
+        mapStyle="https://tiles.openfreemap.org/styles/liberty"
+        style={{ width: '100%', height: '100%' }}
+        onLoad={({ target }) => {
+          mapInstanceRef.current = target;
+          setLoaded(true);
+        }}
+      >
+        {center && (
+          <Marker longitude={center.lng} latitude={center.lat}>
+            <div className="marker-user" title="Your Location" />
+          </Marker>
+        )}
+      </Map>
+    </div>
+  );
+};
+
+export default HistoryMap;   
