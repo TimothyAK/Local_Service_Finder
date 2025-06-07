ed31b5209b5c552b8cb7d9a35c6d34b61150eb21
Rakha Ramiro <rkh_rmiro@MacBookPro.lan>
2025-06-07 19:37:38 +0700

    updated midware

--- front-end/src/api/userAPI.js
+++ front-end/src/api/userAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:8000/api/users'
+const BASE_URL = 'http://localhost:8080/api/user/'
 
 export const loginAPI = async (email, password) => {
         return axios.post(`${BASE_URL}/login/`, 
