8cbbaca990bb25521ab9e7a273cb6df9d0735b82
davidl1459 <davidryanlie01@gmail.com>
2025-06-07 22:00:57 +0700

    Finished User and Amenity API

---
+++ back-end_node/amenity_service/index.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+const amenitiesRoutes = require("./routes/amenities");
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/amenities", amenitiesRoutes);
+
+const PORT = process.env.PORT || 8001;
+app.listen(PORT, () => console.log(`Amenity service running on port ${PORT}`));
