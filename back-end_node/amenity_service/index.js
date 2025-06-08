3cd654c10e384c2340b926a8352c6a9d410c8731
TimothyAK <mothyabraham973@gmail.com>
2025-06-08 15:44:27 +0700

    Added cors header middleware for amenity service

--- back-end_node/amenity_service/index.js
+++ back-end_node/amenity_service/index.js
@@ -1,10 +1,32 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors")
 const amenitiesRoutes = require("./routes/amenities");
 const JWTMiddleware =  require("./middleware/JWTMiddlware")
 
+const corsOptions = {
+    origin: [
+        'http://localhost:5173', 
+        'http://localhost:8000', 
+        'http://localhost:8001', 
+        'http://localhost:8002',
+        "localhost"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: [
+        "accept",
+        "authorization",
+        "content-type",
+        "user-agent",
+        "x-csrftoken",
+        "x-requested-with",
+        "access-token"
+    ]
+}
+
 const app = express();
 app.use(express.json());
+app.use(cors(corsOptions))
 app.use(JWTMiddleware.verifyToken)
 
 app.use("/api/amenities", amenitiesRoutes);
