b5012b17a55f4d7c5e6856be4c8dae7302a62aa4
TimothyAK <mothyabraham973@gmail.com>
2025-06-08 10:09:49 +0700

    Modified user amenty index.js to separate database conneciton code

--- back-end_node/user_amenity_service/index.js
+++ back-end_node/user_amenity_service/index.js
@@ -2,18 +2,16 @@ require("dotenv").config()
 const express = require("express")
 const mongoose = require("mongoose")
 const UserRouter = require("./router/UserAmenityRouter")
+const connectDB = require("./db")
 
-const MONGO_URI = process.env.MONGO_URI
 const PORT = process.env.PORT
 
-mongoose.connect(MONGO_URI)
+const app = express()
 
-mongoose.connection.once("open", () => {
-    const app = express()
+app.use(express.json())
 
-    app.use(express.json())
+connectDB()
 
-    app.use("/api/user_amenities/", UserRouter)
+app.use("/api/user_amenities/", UserRouter)
 
-    app.listen(PORT, () => console.log("server running on port " + PORT))
-})
+app.listen(PORT, () => console.log("server running on port " + PORT))
